9bbcec0cbf484e6f0d61ba233205fab501b7ea38
Daniel Somerfield <dsomerfi@thoughtworks.com>
2017-08-04 14:50:27 -0700

    Fixes to push command

--- pipeline/push_container.sh
+++ pipeline/push_container.sh
@@ -7,9 +7,11 @@ export VAULT_ADDR=http://$VAULT_SERVICE_HOST:$VAULT_SERVICE_PORT
 vault_response=`vault write -format=json auth/approle/login role_id=$VAULT_ROLE_ID secret_id=$VAULT_SECRET_ID`
 token=`echo $vault_response|jq -r '.auth .client_token'`
 
-creds=`VAULT_TOKEN=$token vault read secret/app/pipeline/hello-secret-world-pipeline/registry`
-username=`echo $creds | jq -r '.data .username'
-password=`echo $creds | jq -r '.data .password'
+creds=`VAULT_TOKEN=$token vault read -format=json secret/app/pipeline/hello-secret-world-pipeline/registry`
+username=`echo $creds | jq -r '.data .username'`
+password=`echo $creds | jq -r '.data .password'`
 
+set +e
 docker login -u $username -p $password
 docker push danielsomerfield/hello-secret-world:${GO_PIPELINE_LABEL}
+docker logout
